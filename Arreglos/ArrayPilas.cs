10d96ca2a97813a1e982381ae18eb8e7d0304d38
Jose Alberto Dyck Rincon <jose_dyck@unikino.edu.mx>
2020-07-31 21:53:40 -0700

    DLL

---
+++ Arreglos/ArrayPilas.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arreglos
+{
+    public class ArrayPilas
+    {
+        private string[] array;
+        private int max;
+        private int tope;
+        //constructor
+        public ArrayPilas(int tamanio)
+        {
+            //this hace referencia a lo escrito dentro de la clase
+            this.array = new string[tamanio];
+            this.tope = 0;
+            this.max = array.Length - 1;
+        }
+
+        //validar arreglo no vacio
+
+        private bool ValidaVacio()
+        {
+            return (tope < 1);//Si tope en menor que uno es verdadero
+        }
+
+        private bool ValidaLLeno()
+        {
+            return (tope > max);
+        }
+        //requiere invocarse desde program
+        public void Agregar(string dato)
+        {
+            if (ValidaLLeno())
+            {
+                throw new Exception("Arreglo LLeno");
+            }
+            array[tope] = dato;
+            tope++;
+        }
+        //no recibe parametro dado que la regla de pilas elimina el ultimo ingresado
+        public void Eliminar()
+        {
+            if (ValidaVacio())
+            {
+                throw new Exception("Arreglo Vacio");
+            }
+            tope--;
+            array[tope] = null;//nullo es diferente de vacio
+        }
+        
+        //no recibe parametro pero si regresa un string
+        public string Imprimir()
+        {
+            string dato = string.Empty;
+
+            if (ValidaVacio())
+            {
+                return "Arreglo Vacío";
+            }
+
+            for (int i = 0; i < tope; i++)
+            {
+                if (i>0)
+                {
+                    dato += "\n";
+                }
+                dato += $"[{i}]-{array[i]}";
+            }
+            return dato;
+        }
+    }
+}
--- PilasColas/Program.cs
+++ PilasColas/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Arreglos;
+using System;
 
 namespace PilasColas
 {
@@ -6,7 +7,58 @@ namespace PilasColas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                ArrayPilas pila = new ArrayPilas(5);
+
+                pila.Agregar("cero");
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Agregar("uno");
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Agregar("dos");
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Agregar("tres");
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Agregar("cuatro");
+                Console.WriteLine(pila.Imprimir());
+
+                //Console.WriteLine("---------");
+                //pila.Agregar("cinco");
+                //Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Eliminar();
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Eliminar();
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Eliminar();
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Eliminar();
+                Console.WriteLine(pila.Imprimir());
+
+                Console.WriteLine("---------");
+                pila.Eliminar();
+                Console.WriteLine(pila.Imprimir());
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
         }
     }
 }
